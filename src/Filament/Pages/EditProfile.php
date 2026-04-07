e9ff6f4a18bd7048203e0f912037136e086427d9
alexisgatuingt <hello@lapetiteagencelandaise.com>
2026-04-07 18:10:13 +0200

    fix: update EditProfile namespace for Filament 5 (Auth\Pages)

--- src/Filament/Pages/EditProfile.php
+++ src/Filament/Pages/EditProfile.php
@@ -5,7 +5,7 @@ namespace Alexisgt01\CmsCore\Filament\Pages;
 use Filament\Forms\Components\Component;
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
-use Filament\Pages\Auth\EditProfile as BaseEditProfile;
+use Filament\Auth\Pages\EditProfile as BaseEditProfile;
 
 class EditProfile extends BaseEditProfile
 {
